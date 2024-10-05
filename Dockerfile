095ec61b92eeaa3711a7b3baa9a3a6ab249832f2
Bryan Jiang <haozhoujiang@gmail.com>
2024-10-04 20:58:57 -0400

    fix: fixed Dockerfile and env variables for deployment

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,27 @@
-# Use an official Python runtime as a parent image
-FROM python:3.12.6-slim
+# Build frontend 
 
-# Set the working directory in the container
+FROM node:18-alpine AS frontend-build
+
 WORKDIR /app
-
-# Copy the current directory contents into the container at /app
 COPY . .
 
-# Install any needed packages specified in requirements.txt
-RUN pip3 install --no-cache-dir -r requirements.txt
+WORKDIR /app/client
+RUN npm install
+RUN npm run build
+
+
+FROM python:3.12.6-slim
+
+WORKDIR /app 
+COPY . .
+
+WORKDIR /app/server
+RUN pip3 install -r requirements.txt
+
+WORKDIR /app
+
+# Copy the built frontend files from the frontend-build stage to the backend container
+COPY --from=frontend-build /app/client/dist /app/dist
 
 # Make port 8080 available to the world outside this container
 EXPOSE 8080
@@ -17,4 +30,4 @@ EXPOSE 8080
 ENV PORT=8080
 
 # Run the application using Gunicorn
-CMD ["gunicorn", "-b", ":8080", "app:app"]
+CMD ["gunicorn", "-b", ":8080", "server.app:app"]
--- server/app.py
+++ server/app.py
@@ -1,23 +1,37 @@
 import os 
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-from gemini import gemini_generate, gemini_new_chat
+from .gemini import gemini_generate, gemini_new_chat
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/dist')
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config['CORS_HEADERS'] = 'Content-Type' # Allows CORS for JSON resources
 app.config.from_mapping(SECRET_KEY=os.getenv('FLASK_SECRET_KEY'))
 
+
+# Serve the frontend
+@app.route('/')
+def serve_frontend():
+   return send_from_directory(app.static_folder, 'index.html')
+
+# Serve static assets
+@app.route('/<path:path>')
+def serve_static_files(path):
+   return send_from_directory(app.static_folder, path)
+
+
+# Create an empty new chat session
 @app.route('/new_chat', methods=['POST'])
 def new_chat():
   session_id = gemini_new_chat()
   return jsonify({'session_id': session_id}), 200
 
+# Generate a response from the LLM
 @app.route('/generate', methods=['POST'])
 def generate():  
   data = request.json
